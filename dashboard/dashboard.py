28ba393df5b3feb2ed4b20e8d07d48f51c7ec287
aldakurniaputri <160564920+aldakurniaputri@users.noreply.github.com>
2024-03-12 18:49:18 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,8 +5,20 @@ import streamlit as st
 from babel.numbers import format_currency
 
 # Load data
-day_df = pd.read_csv("https://raw.githubusercontent.com/aldakurniaputri/Tugas_Akhir/main/dashboard/day_data.csv")
-hour_df = pd.read_csv("https://raw.githubusercontent.com/aldakurniaputri/Tugas_Akhir/main/dashboard/hour_data.csv")
+day_df = pd.read_csv("day_data.csv")
+hour_df = pd.read_csv("hour_data.csv")
+
+# Membagi data menjadi beberapa kelompok suhu yaitu suhu rendah, sedang, dan tinggi berdasarkan kuartil
+def categorize_by_temperature(temp):
+    if temp < day_df['temp'].quantile(0.25):
+        return 'rendah'
+    elif temp < day_df['temp'].quantile(0.75):
+        return 'sedang'
+    else:
+        return 'tinggi'
+
+# Menambahkan kolom kategori suhu
+day_df['temp_category'] = day_df['temp'].apply(categorize_by_temperature)
 
 
 # Set style
@@ -25,33 +37,9 @@ with st.sidebar:
         menunjukkan pola persewaan sepeda rentang waktu bulanan."""
     )
 
-tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
+tab1, tab2, tab3, tab4 = st.tabs(["Tab 1", "Tab 2", "Tab 3", "Tab 4"])
  
 with tab1:
-    st.header('Jumlah Penyewaan Sepeda berdasarkan Musim:sparkles:')
-
-    # Plot rata-rata jumlah penyewaan sepeda berdasarkan musim
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.barplot(data=day_df, x='season', y='cnt', ci=None, ax=ax)
-    ax.set_title('Rata-rata Jumlah Penyewaan Sepeda Berdasarkan Musim')
-    ax.set_xlabel('Musim')
-    ax.set_ylabel('Rata-rata Jumlah Penyewaan Sepeda')
-    ax.set_xticks([0, 1, 2, 3])
-    ax.set_xticklabels(['Spring', 'Summer', 'Fall', 'Winter'])
-
-    # Display plot
-    st.pyplot(fig)
-
-    with st.expander("Baca Penjelasan Grafik"):
-        st.write(
-            """Berdasarkan visualisasi grafik pola persewaan sepeda pada setiap musim di atas, 
-            terlihat bahwa musim panas (summer) dan musim gugur (fall) cenderung memiliki jumlah penyewaan sepeda 
-            yang lebih tinggi daripada musim semi (spring) dan musim dingin (winter).
-            Hal ini mungkin disebabkan oleh kondisi cuaca yang lebih menyenangkan bagi aktivitas luar ruangan
-            selama musim panas dan kebutuhan transportasi alternatif selama musim dingin."""
-    )
- 
-with tab2:
     from matplotlib.dates import DateFormatter
     day_df['dteday'] = pd.to_datetime(day_df['dteday'])
     day_df.set_index('dteday', inplace=True)  # Set 'dteday' as index
@@ -83,7 +71,7 @@ with tab2:
             terkait permintaan penyewaan sepeda."""
     )
  
-with tab3:
+with tab2:
     st.header('Persebaran Jumlah Penyewaan Sepeda pada Hari Libur dan Hari Kerja:sparkles:')
 
     # Plot persebaran jumlah penyewaan sepeda
@@ -106,4 +94,47 @@ with tab3:
             menunjukkan bahwa rata-rata jumlah penyewaan sepeda lebih tinggi pada hari libur daripada pada hari kerja.
             Persebaran pada kategori "Hari Libur" lebih besar daripada pada kategori "Bukan Hari Libur",
             menunjukkan variasi yang lebih besar dalam jumlah penyewaan sepeda pada hari libur."""
+    )
+
+with tab3:
+    st.header('Jumlah Penyewaan Sepeda berdasarkan Musim:sparkles:')
+
+    # Plot rata-rata jumlah penyewaan sepeda berdasarkan musim
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(data=day_df, x='season', y='cnt', ci=None, ax=ax)
+    ax.set_title('Rata-rata Jumlah Penyewaan Sepeda Berdasarkan Musim')
+    ax.set_xlabel('Musim')
+    ax.set_ylabel('Rata-rata Jumlah Penyewaan Sepeda')
+    ax.set_xticks([0, 1, 2, 3])
+    ax.set_xticklabels(['Spring', 'Summer', 'Fall', 'Winter'])
+
+    # Display plot
+    st.pyplot(fig)
+
+    with st.expander("Baca Penjelasan Grafik"):
+        st.write(
+            """Berdasarkan visualisasi grafik pola persewaan sepeda pada setiap musim di atas, 
+            terlihat bahwa musim panas (summer) dan musim gugur (fall) cenderung memiliki jumlah penyewaan sepeda 
+            yang lebih tinggi daripada musim semi (spring) dan musim dingin (winter).
+            Hal ini mungkin disebabkan oleh kondisi cuaca yang lebih menyenangkan bagi aktivitas luar ruangan
+            selama musim panas dan kebutuhan transportasi alternatif selama musim dingin."""
+    )
+        
+with tab4:
+    st.header('Preferensi penyewa sepeda terhadap tingkatan suhu:sparkles:')
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.boxplot(x='temp_category', y='cnt', data=day_df, palette='pastel', ax=ax)
+    ax.set_title('Distribusi Jumlah Penyewaan Sepeda Berdasarkan Kategori Suhu')
+    ax.set_xlabel('Kategori Suhu')
+    ax.set_ylabel('Jumlah Penyewaan Sepeda')
+    
+    st.pyplot(fig)
+    
+    with st.expander("Baca Penjelasan Grafik"):
+        st.write(
+            """Berdasarkan visualisasi suhu yang dikelompokkan menjadi suhu rendah, sedang, dan tinggi
+            terlihat pengguna cenderung menggunakan sepeda lebih sering saat suhu tinggi dibandingkan dengan suhu sedang atau rendah. 
+            Hal ini selaras dengan analisis kita sebelumnya mengenai musim panas dan musim gugur (bersuhu tinggi) yang memiliki
+            jumlah penyewaan sepeda lebih banyak dibanding ketika musim semi dan musim dingin."""
     )
